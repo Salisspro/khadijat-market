3f9e1c47d6d1d85ca5a607b513806a4901d09756
salisspro <salisspro@gmail.com>
2025-05-07 07:39:36 +0100

    morning change

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import IMG from '../../assets/img/1746336704509.jpg'
-import IMG2 from '../../assets/image/s-l1600.webp'
+import IMG2 from '../../assets/image/images.jpg'
 import IMG3 from '../../assets/image/xiaomi-redmi-a3-4g-64gb-schwarz-ohne-simlock-ohne-branding-001.jpg'
 import IMG4 from '../../assets/image/614gnlfQt2L._AC_SL1500_.jpg'
 
@@ -17,34 +17,28 @@ export default function Hero() {
         <button className='bg-slate-500 text-white p-2 rounded-md mt-4'>See our products</button>
       </div>
     
-      <div className='flex flex-col justify-center items-center mt-[3em]'>
+      <div className='flex flex-col justify-center items-center mt-[3em] '>
         <h1>Our best sellers</h1>
         <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mt-5 border-2 border-slate-500 p-5'>
           <div>
+            <img className='h-[13em]' src={IMG2} alt="" />
             <p className='text-slate-500 '>Redmi A3  </p>
-            <img className='' src={IMG2} alt="" />
-            <p>Price: 1000</p>
           
 
             </div>
           <div>
+            <img className='h-[13em]' src={IMG3} alt="" />
             <p className='text-slate-500'>Redmi A3  </p>
-            <img className='' src={IMG3} alt="" />
-            <p>Price: 1000</p>
 
 
           </div>
           <div>
+            <img className='h-[13em]' src={IMG4} alt=""/>
             <p className='text-slate-500'>women dress  </p>
-            <img className='' src={IMG4} alt=""/>
-            <p>Price: 1000</p>
             </div>
           <div>
-            <p className='text-slate-500'>
-              Milks cake
-            </p>
-            <img className='' src={IMG5} alt="" />
-            <p>Price: 1000</p>
+            <img className='h-[13em]' src={IMG5} alt="" />
+            <p className='text-slate-500'>Milks cake</p>
             </div>
           <a href="">
             <button className='text-blue-500'>See all</button>
