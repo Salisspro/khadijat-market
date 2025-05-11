3b25d6c93e1c5440f578c8b5e4f3019df93bbf8a
salisspro <salisspro@gmail.com>
2025-05-11 21:26:19 +0100

    new update

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,51 +1,49 @@
 
-import React from 'react'
-import IMG from '../../assets/img/1746336704509.jpg'
-import IMG2 from '../milkImages/image_downloader_1746944010113.jpg'
+import IMG2 from '../milkImages/image_downloader_1746943896125.jpg'
 import IMG3 from '../../assets/image/xiaomi-redmi-a3-4g-64gb-schwarz-ohne-simlock-ohne-branding-001.jpg'
 import IMG4 from '../shoes/image_downloader_1746945011981.jpg'
 
-import IMG5 from '../../assets/image/PDM127P.webp'
+import IMG5 from '../../assets/image/614gnlfQt2L._AC_SL1500_.jpg'
 
 
 export default function Hero() {
   return (
     <div className='w-full text-slate-700'>
-      <div className='flex flex-col justify-center items-center mt-[3em] '>
-        <h1 className='mt-20 text-3xl font-bold'>Our best sellers</h1>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='mt-20 text-3xl font-bold capitalize'>Our best sellers</h1>
         <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mt-5  p-3 '>
-          <div className='bg-gradient-to-t to-blue-200 from-orange-300 p-3 rounded'>
+          <div className='border p-2 rounded'>
             <a href="/Milks">
               <img className='rounded-[10px] w-full h-[13em]' src={IMG2} alt="" />
-              <p className=''>Milks</p>
+              <p className='text-[19px] my-2 font-bold'>Milks</p>
             </a>
           </div>
-          <div className='bg-gradient-to-t to-blue-200 from-orange-300 p-3 rounded'>
+          <div className='border p-2 rounded'>
             <a href="/Phones">
 
               <img className='rounded-[10px] w-full h-[13em]' src={IMG3} alt="" />
-              <p className=''>Phones</p>
+              <p className='text-[19px] my-2 font-bold'>Phones</p>
             </a>
           </div>
 
-          <div className='bg-gradient-to-t to-blue-200 from-orange-300 p-3 rounded'>
+          <div className='border p-2 rounded'>
             <a href="/Shoes">
 
             <img className='rounded-[10px] w-full h-[13em]' src={IMG4} alt="" />
-            <p className=''>Shoes</p>
+            <p className='text-[19px] my-2 font-bold'>Shoes</p>
             </a>
           </div>
 
-          <div className='bg-gradient-to-t to-blue-200 from-orange-300 p-3 rounded'>
+          <div className='border p-2 rounded'>
             <a href="/GirlsDress">
 
               <img className='rounded-[10px] w-full h-[13em]' src={IMG5} alt="" />
-              <p className=''>Girls items</p>
+              <p className='text-[19px] my-2 font-bold '>Girls items</p>
             </a>
           </div>
 
           <a href="/SeeAll">
-            <button className='text-blue-500'>See all</button>
+            <button className='text-blue-500 text-[18px] font-bold ml-2'>See all</button>
           </a>
         </div>
       </div>
--- src/components/router/Milks.jsx
+++ src/components/router/Milks.jsx
@@ -10,13 +10,36 @@ import IMG4 from '../milkImages/image_downloader_1746944010113.jpg'
 
 export default function Milks() {
    return (
-      <div className='grid grid-flow-row gap-5 p-10'>
-         <div>
+      <div className='grid grid-flow-row gap-5 p-5'>
+         <div className='border p-2'>
             <img className='rounded w-full h-[17em]' src={IMG1} alt="" />
 
+            <div>
+               <div className='flex items-center mt-5 justify-between '
+               >
+                  <div>
+                     <p className='text-[19px] font-bold text-slate-800 '>
+                        <span className='text-orange-500 text-4xl font-semibold'>10</span>
+                        Piececes
+                     </p>
+                  </div>
+               
+
+               </div>
+
+               <div className='flex  gap-5 text-[17px] font-semibold'>
+                  <div className='mt-1'>
+                     <div className=' grid grid-cols-6 gap-10 font-bold text-slate-700'>
+                        <del className='text-2xl text-red-700 '>$10</del>
+                        <span className='text-4xl text-blue-500 '>$5</span>
+                        <button className='bg-orange-500 w-full p-2 col-span-4 rounded'>BUY</button>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
 
          </div>
-
          <div>
             <img className='rounded w-full h-[17em]' src={IMG2} alt="" />
 
