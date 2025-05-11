de26cfe8b2ee5cadef2485fd9b090efbaa221b4b
salisspro <salisspro@gmail.com>
2025-05-11 21:34:09 +0100

    new update

--- src/components/router/Milks.jsx
+++ src/components/router/Milks.jsx
@@ -23,7 +23,7 @@ export default function Milks() {
                         Piececes
                      </p>
                   </div>
-               
+
 
                </div>
 
@@ -40,19 +40,91 @@ export default function Milks() {
             </div>
 
          </div>
-         <div>
+         <div className='border p-2'>
             <img className='rounded w-full h-[17em]' src={IMG2} alt="" />
 
+            <div>
+               <div className='flex items-center mt-5 justify-between '
+               >
+                  <div>
+                     <p className='text-[19px] font-bold text-slate-800 '>
+                        <span className='text-orange-500 text-4xl font-semibold'>10</span>
+                        Piececes
+                     </p>
+                  </div>
+
+
+               </div>
+
+               <div className='flex  gap-5 text-[17px] font-semibold'>
+                  <div className='mt-1'>
+                     <div className=' grid grid-cols-6 gap-10 font-bold text-slate-700'>
+                        <del className='text-2xl text-red-700 '>$10</del>
+                        <span className='text-4xl text-blue-500 '>$5</span>
+                        <button className='bg-orange-500 w-full p-2 col-span-4 rounded'>BUY</button>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
 
-         <div>
+         <div className='border p-2'>
             <img className='rounded w-full h-[17em]' src={IMG3} alt="" />
 
+            <div>
+               <div className='flex items-center mt-5 justify-between '
+               >
+                  <div>
+                     <p className='text-[19px] font-bold text-slate-800 '>
+                        <span className='text-orange-500 text-4xl font-semibold'>10</span>
+                        Piececes
+                     </p>
+                  </div>
+
+
+               </div>
+
+               <div className='flex  gap-5 text-[17px] font-semibold'>
+                  <div className='mt-1'>
+                     <div className=' grid grid-cols-6 gap-10 font-bold text-slate-700'>
+                        <del className='text-2xl text-red-700 '>$10</del>
+                        <span className='text-4xl text-blue-500 '>$5</span>
+                        <button className='bg-orange-500 w-full p-2 col-span-4 rounded'>BUY</button>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
 
-         <div>
+         <div className='border p-2'>
             <img className='rounded w-full h-[17em]' src={IMG4} alt="" />
 
+            <div >
+               <div className='flex items-center mt-5 justify-between '
+               >
+                  <div>
+                     <p className='text-[19px] font-bold text-slate-800 '>
+                        <span className='text-orange-500 text-4xl font-semibold'>10</span>
+                        Piececes
+                     </p>
+                  </div>
+
+
+               </div>
+
+               <div className='flex  gap-5 text-[17px] font-semibold'>
+                  <div className='mt-1'>
+                     <div className=' grid grid-cols-6 gap-10 font-bold text-slate-700'>
+                        <del className='text-2xl text-red-700 '>$10</del>
+                        <span className='text-4xl text-blue-500 '>$5</span>
+                        <button className='bg-orange-500 w-full p-2 col-span-4 rounded'>BUY</button>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
 
       </div>
